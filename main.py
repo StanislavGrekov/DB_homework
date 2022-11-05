7da275dfcf3ea6a9666c6e08d33f27fc1898e217
Grekov Stanislav <stas.ik1987@yandex.ru>
2022-11-05 14:27:15 +0500

    ADD HomeWork 5

--- main.py
+++ main.py
@@ -18,10 +18,7 @@ def create_table(cursor):
         CREATE TABLE IF NOT EXISTS phone(
         	id SERIAL PRIMARY KEY,
             phone_book_id INTEGER NOT NULL REFERENCES phone_book(id),
-            phone_1 INTEGER,
-            phone_2 INTEGER,
-            phone_3 INTEGER,
-            phone_4 INTEGER
+            phone_ INTEGER UNIQUE
             );
                 ''')
     conn.commit()
@@ -50,15 +47,6 @@ def insert(cursor, last_name_, first_name_, email_):
                 INSERT INTO phone_book(email, last_name, first_name, date_insert)
                 VALUES (%s, %s, %s, %s);
         ''', (email_, last_name_, first_name_, current_date))
-        cursor.execute('''
-                 SELECT id FROM phone_book
-                 WHERE last_name = %s;
-         ''', (last_name_,))
-        answer = cursor.fetchall()
-        cursor.execute('''
-                INSERT INTO phone (phone_book_id, phone_1, phone_2, phone_3, phone_4)
-                VALUES (%s, %s, %s, %s, %s);
-        ''', (answer[0],0, 0, 0, 0))
         conn.commit()
         print(f'Запись с фамилией {last_name_} добавлена в базу.\n')
 
@@ -73,39 +61,11 @@ def add_phone(cursor, last_name_, phone_ ):
     ''', (last_name_,))
     id = cursor.fetchall()[0]
     cursor.execute('''
-        SELECT phone_1, phone_2,phone_3, phone_4 FROM phone
-        WHERE phone_book_id = %s;
-    ''', (id,))
-    answer = cursor.fetchall()
-    phone_ = int(phone_)
-    for element in answer:
-        phone_1, phone_2, phone_3, phone_4 =element[0], element[1], element[2], element[3]
-        if phone_1 == phone_  or phone_2 == phone_ or phone_3 == phone_ or phone_4 == phone_: # Проверка на совпадение номера телефона
-            print('Такой телефон у данного клиента уже есть, введите другой!')
-        else:
-            if phone_1 == 0:
-                cursor.execute("""
-                      UPDATE phone SET phone_1 = %s WHERE phone_book_id=%s;
-                      """, (phone_, id))
-                print(f'Телефонный номер {phone_} клиенту {last_name_} добавлен.')
-            elif phone_1 != 0 and phone_2 == 0:
-                cursor.execute("""
-                      UPDATE phone SET phone_2 = %s WHERE phone_book_id=%s;
-                      """, (phone_, id))
-                print(f'Телефонный номер {phone_} клиенту {last_name_} добавлен.')
-            elif phone_1 != 0 and phone_2 != 0 and phone_3 == 0:
-                cursor.execute("""
-                      UPDATE phone SET phone_3 = %s WHERE phone_book_id=%s;
-                      """, (phone_, id))
-                print(f'Телефонный номер {phone_} клиенту {last_name_} добавлен.')
-            elif phone_1 != 0 and phone_2 != 0 and phone_3 != 0 and phone_4 == 0:
-                cursor.execute("""
-                      UPDATE phone SET phone_4 = %s WHERE phone_book_id=%s;
-                      """, (phone_, id))
-                print(f'Телефонный номер {phone_} клиенту {last_name_} добавлен.')
-            elif phone_1 != 0 and phone_2 != 0 and phone_3 != 0 and phone_4 != 0:
-                print('К сожалению больше телефонов клиенту добавить нельзя.')
-            conn.commit()
+        INSERT INTO phone(phone_book_id, phone_)
+        VALUES(%s,%s);
+    ''', (id,phone_))
+    conn.commit()
+    print(f'Телефонный номер {phone_} клиенту {last_name_} добавлен.')
 
 
 def change_clients(cursor, last_name_, last_name_new, first_name_new, email_new):
@@ -126,28 +86,22 @@ def delete_phone(cursor, last_name_, phone_):
     Функция, позволяющая удалить телефон для существующего клиента.
     '''
     cursor.execute('''
-        SELECT phone_book_id, phone_1, phone_2,phone_3, phone_4 FROM phone ph
+        SELECT phone_book_id, phone_ FROM phone ph
         INNER JOIN phone_book pb ON pb.id = ph.phone_book_id 
         WHERE pb.last_name = %s;
     ''', (last_name_,))
     answer = cursor.fetchall()
     phone_ = int(phone_)
-    answer_phone = [answer[0][1], answer[0][2], answer[0][3], answer[0][4]]
-    list_ = []
-    for element in answer_phone:
-            if element != phone_:
-                list_.append(element)
-            else:
-                list_.append('0')
-    my_tuple = tuple(list_)
-    if phone_ in answer_phone:
-        cursor.execute("""
-                UPDATE phone SET phone_1 = %s, phone_2 = %s, phone_3 = %s, phone_4 = %s WHERE phone_book_id = %s;
-                """, (my_tuple[0], my_tuple[1], my_tuple[2], my_tuple[3], answer[0][0]))
-        conn.commit()
-        print(f'Телефонный номер {phone_}  клиентa {last_name_} успешно удален.')
-    else:
-        print(f'Телефона {phone_} у клиента {last_name_} нет.')
+    # answer_phone = [answer[0][1], answer[0][2]]
+    for element in answer:
+        if phone_ == element[1]:
+            cursor.execute("""
+                    DELETE FROM phone
+                    WHERE phone_ = %s;
+                    
+                    """, (element[1],))
+            conn.commit()
+            print(f'Телефонный номер {phone_}  клиентa {last_name_} успешно удален.')
 
 
 def delete_client(cursor, last_name_):
@@ -171,23 +125,24 @@ def delete_client(cursor, last_name_):
         print(f'Сотрудника с фамилией {last_name_} не найдено.')
 
 
-def find_client_name(cursor, last_name_, first_name_, email_):
+def find_client_name(cursor, last_name_):
     '''
     Функция, позволяющая найти клиента по его данным (имени, фамилии, email-у).
     '''
-    try:
-        cursor.execute("""
-                SELECT last_name, first_name, email, date_insert, phone_1,phone_2,phone_3,phone_4 FROM phone_book pb
-                INNER JOIN phone p ON pb.id = p.phone_book_id
-                WHERE last_name = %s AND first_name = %s AND email = %s;
-                """, (last_name_, first_name_, email_))
-        answer = cursor.fetchall()
-        conn.commit()
-        print(f'Фамилия: {answer[0][0]}, Имя: {answer[0][1]}, почта: {answer[0][2]}, дата занесения информации: {answer[0][3]}, '
-                  f'тел.номер.№1: {answer[0][4]}, тел.номер.№2: {answer[0][5]}, тел.номер.№3: {answer[0][6]}, тел.номер.№4: {answer[0][7]} '
-                  f'(Примечание - если телефонный номер - 0, значит он не задан.)')
-    except:
-        print('Информации по указанному сотруднику нет.')
+    cursor.execute("""
+                    SELECT last_name, first_name, email, date_insert, phone_ FROM phone_book pb
+                    INNER JOIN phone p ON pb.id = p.phone_book_id
+                    WHERE last_name = %s;
+                    """, (last_name_,))
+    answer = cursor.fetchall()
+    list_phone = []
+    for element in answer:
+            list_phone.append(element[4])
+    conn.commit()
+    str_List_phone = [str(i) for i in list_phone]
+    print(f'Фамилия: {answer[0][0]}, Имя: {answer[0][1]}, почта: {answer[0][2]}, дата занесения информации: {answer[0][3]}, '
+                      f'тел.номера: {",".join(str_List_phone)}')
+
 
 
 def find_client_phone(cursor, phone_):
@@ -196,10 +151,10 @@ def find_client_phone(cursor, phone_):
     '''
     try:
         cursor.execute("""
-            SELECT last_name, first_name, email, date_insert, phone_1,phone_2,phone_3,phone_4 FROM phone_book pb
+            SELECT last_name, first_name, email, date_insert, phone_ FROM phone_book pb
             INNER JOIN phone p ON pb.id = p.phone_book_id
-            WHERE phone_1 = %s OR phone_2 = %s OR phone_3 = %s OR phone_4 = %s;
-            """, (phone_, phone_, phone_, phone_))
+            WHERE phone_ = %s;
+            """, (phone_,))
         answer = cursor.fetchall()
         conn.commit()
         print(f'Фамилия: {answer[0][0]}, Имя: {answer[0][1]}, почта: {answer[0][2]}, дата занесения информации: {answer[0][3]},')
@@ -256,14 +211,14 @@ if __name__ == '__main__':
                     'Введите фамилию клиента, информацию по которому вы хотите удалить\n')
                 delete_client(cursor, last_name_)
             elif key == 'fc':
-                key_next = input('Введите name, если хотите произвести поиск по фамилии, имени и э.почте или введите'
+                key_next = input('Введите name, если хотите произвести поиск по фамилии или введите'
                                  ' phone, если хотите выполнить поиск по телефону. Можете ввести q для '
                                  'выхода в предыдущее меню.\n')
                 while True:
                     if key_next == 'name':
-                        last_name_, first_name_, email_ = input(
-                            'Введите фамилию, имя, эл. почту через пробел.\n').split()
-                        find_client_name(cursor, last_name_, first_name_, email_)
+                        last_name_ = input(
+                            'Введите фамилию:\n')
+                        find_client_name(cursor, last_name_)
                         break
                     elif key_next == 'phone':
                         phone_ = int(input(
